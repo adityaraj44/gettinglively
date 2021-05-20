a5dbecb484fe78b9e5f2587355693787d9311219
Aditya Raj <brockjuly852@gmail.com>
2021-05-20 16:59:41 +0530

    img changed

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 ScrollReveal().reveal(".tagline1", { delay: 500 });
 ScrollReveal().reveal(".tagline2", { delay: 750 });
 ScrollReveal().reveal(".tagline3", { delay: 1000 });
-ScrollReveal().reveal(".tagline4", { delay: 1250 });
+ScrollReveal().reveal(".tagline4", { delay: 1150 });
 
 //aos
 AOS.init();
