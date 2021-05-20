7168c44906a457b88b64ddfca011f0ff6ad601be
Aditya Raj <brockjuly852@gmail.com>
2021-05-20 19:36:53 +0530

    homepage redesign and image optimisation

--- public/main.js
+++ public/main.js
@@ -6,3 +6,14 @@ ScrollReveal().reveal(".tagline4", { delay: 1150 });
 
 //aos
 AOS.init();
+
+ScrollReveal().reveal(".food1", { delay: 100 });
+ScrollReveal().reveal(".food2", { delay: 200 });
+ScrollReveal().reveal(".food3", { delay: 300 });
+ScrollReveal().reveal(".food4", { delay: 400 });
+ScrollReveal().reveal(".food5", { delay: 500 });
+ScrollReveal().reveal(".food6", { delay: 600 });
+
+ScrollReveal().reveal(".food7", { delay: 700 });
+ScrollReveal().reveal(".food8", { delay: 800 });
+ScrollReveal().reveal(".food9", { delay: 900 });
