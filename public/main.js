53d2f4c0aca3b0e8004fee693c6598ed0b319cd3
Aditya Raj <brockjuly852@gmail.com>
2021-05-22 20:57:00 +0530

    user auth and reset password feature added

--- public/main.js
+++ public/main.js
@@ -6,21 +6,3 @@ ScrollReveal().reveal(".tagline4", { delay: 1150 });
 
 //aos
 AOS.init();
-
-//places
-$(document).ready(() => {
-  var autocomplete;
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("searchinput"),
-    {
-      types: ["geocode"],
-      componentRestrictions: {
-        country: "uk",
-      },
-    }
-  );
-
-  google.maps.event.addEventListener(autocomplete, "place_changed", () => {
-    var near_place = autocomplete.getPlace();
-  });
-});
